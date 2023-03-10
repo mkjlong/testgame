78bf1d698c6aab601192bff564d8bca97b12e5a6
hortema <darkgamedev.gg@gmail.com>
2023-03-10 12:13:06 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,11 @@
 class Player {
   constructor(h, w, x, y, xv, yv, type) {
-    this.height = h;
-    this.width = w;
+    this.height = h
+    this.width = w
+    this.x = x
+    this.y = y
+    this.xv = xv
+    this.yv = yv
+    this.powerup = type
   }
 }
